e44175be78a2b99929fbedc8c52c25422eef23d7
Jake Samuels <samuels242@googlemail.com>
2022-06-23 00:01:16 +0100

    Codewars solutions

--- JavaScript/8kyu/8kyu.js
+++ JavaScript/8kyu/8kyu.js
@@ -107,10 +107,8 @@ function repeatStr(n, s) {
   return str;
 }
 
+// Sentence Smash
+// https://www.codewars.com/kata/53dc23c68a0c93699800041d
 function smash(words) {
-  return words.join("");
-}
-
-function smash(words) {
-  return words.join("");
+  return words.join(" ");
 }
