1bd84a49ffc0bdae7e346613d62449a9272ec55d
Jake Samuels <samuels242@googlemail.com>
2022-06-28 23:34:12 +0100

    JavaScript 7kyu - Highest and Lowest

--- JavaScript/7kyu/7kyu.js
+++ JavaScript/7kyu/7kyu.js
@@ -50,4 +50,10 @@ function getDivisorsCnt(n) {
   }
   return count;
 }
-getDivisorsCnt(11);
+
+// Highest and Lowest - 28/06/2022
+// https://www.codewars.com/kata/554b4ac871d6813a03000035
+function highAndLow(numbers) {
+  const arr = numbers.split(" ").sort((a, b) => a - b);
+  return [arr[arr.length - 1], arr[0]].join(" ");
+}
